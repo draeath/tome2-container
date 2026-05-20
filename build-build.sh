71c05fdfadfc6cf314f7b62da22dd6d2bcf6581c
Paul Bransford <paul.bransford@epi.usf.edu>
2026-05-20 11:38:26 -0400

    use lzo, drop ccache

--- build-build.sh
+++ build-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-apptainer build --mksquashfs-args "-comp zstd -Xcompression-level 22" -F build.sif build.def
+apptainer build --mksquashfs-args "-comp lzo" -F build.sif build.def
--- build-tome.sh
+++ build-tome.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 if test -f tome.tar; then
   unlink tome.tar
 fi
-apptainer build --mksquashfs-args "-comp zstd -Xcompression-level 22" -F --mount "type=bind,src=${CCACHE_DIR:-$HOME/.cache/ccache},dst=/tmp/.ccache" tome.sif tome.def
+apptainer build --mksquashfs-args "-comp lzo" -F tome.sif tome.def
